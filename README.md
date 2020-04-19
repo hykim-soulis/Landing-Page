cdf0b42a2dd7b37c67757b042c609874107c4041
hykim-soulis <hyeyoung9002@gmail.com>
2020-04-19 12:40:11 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Landing Page Project
+
+## Project Scope
+
+This Landing Page project is for Udacity's front end nanodegree program. Burner CSS and html code was provided by the course. I have implemented a navigation panel that smoothly scrolls the user to the relative section in the landing page.
+
+## Instructions for Running Project
+
+To run this project, simply download the folder and open the index.html file in a browser. The index.html file is located in the root directory of the project.
+
+
---
+++ js/app.js
@@ -0,0 +1,72 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+
+const nav_ul = document.getElementById("navbar__list");
+
+const sections = document.querySelectorAll("section");
+const sections_len = sections.length;
+
+
+
+
+//Nav function
+function makeNav(el){
+    for (let i = 0; i < sections_len; i++) {
+        const container = document.createElement('li');
+        container.className = "menu__link";
+        container.textContent = `Section${i+1}`;
+        el.appendChild(container);
+    }
+}
+
+makeNav(nav_ul);
+
+//Event handler function
+/* 
+    -Identify which list element was clicked and get it.
+    -get the offsetTop of the element and scroll to that position
+    -add the active class to the section
+*/
+function respondToTheClick(evt) {
+    const text = evt.target.textContent;
+    if (text.length < 10) {
+        num = text.slice(-1);
+        scroll({
+            top: document.getElementById(`section${num}`).offsetTop,
+            behavior: 'smooth'
+        });
+        toggleClass(num);
+    }
+}
+
+nav_ul.addEventListener('click', respondToTheClick)
+
+
+/* 
+    add the active class to the clicked section and remove it from 
+    all other sections 
+*/
+function toggleClass(n){
+    console.log(sections_len)
+    let sec_index = n-1;
+    for(i = 0; i < sections_len ; i++){
+        if (i === sec_index) {
+            document.getElementById(`section${n}`).classList.add('your-active-class');
+        } else {
+            document.getElementById(`section${i+1}`).classList.remove('your-active-class');
+        }
+    }
+}
